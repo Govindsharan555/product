4c7e60a707ac35dc14a7d5874f6354cdf5262ee8
Govindsharan555 <govindsharan85@gmail.com>
2025-02-19 20:13:15 +0530

    added message

--- src/main/java/dev/Govind/productcatalog/services/ProductService.java
+++ src/main/java/dev/Govind/productcatalog/services/ProductService.java
@@ -3,6 +3,8 @@ package dev.Govind.productcatalog.services;
 import dev.Govind.productcatalog.Model.Product;
 import dev.Govind.productcatalog.dtos.genericproductdto;
 
+//Govindsharan
+
 public interface ProductService {
 
      genericproductdto getproductbyid(Long id);
